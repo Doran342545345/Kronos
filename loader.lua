6c47aa63d15d3bc89776cd736596471c98906dbe
Doran <105158686+Doran342545345@users.noreply.github.com>
2026-08-28 20:06:45 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,15 +1,6 @@
-local url = "https://raw.githubusercontent.com/Doran342545345/Kronos/refs/heads/main/Games"
-
-local games = {
+return = {
 	[90568084448279] = "One_Tap"; -- GameID, script name
     
 	
 	
 }
-
-for i,v in next, games do
-    games[i] = table.concat(v:split(' '), '_')
-end
-
-local name = games[game.PlaceId] or games[game.GameId]
-return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
